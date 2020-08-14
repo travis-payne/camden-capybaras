1493a843044ad38de169d3a97f71946f8396a431
Travis Payne <travis.payne@uea.ac.uk>
2020-08-14 13:40:08 +0100

    Added material UI blog template

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from 'components/Navigation/Navigation';
+import Blog from 'newsite/Blog';
 import './App.css';
 
 class App extends Component {
 
   render() {
     return (
-        <Navigation />
+        <Blog />
     );
   }
 
