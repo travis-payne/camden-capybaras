5fca6d14c95bffa53023d731d754e16a579dac22
Travis Payne <travis.payne@accenture.com>
2022-06-26 14:25:11 +0100

    Implementing lambda call to trigger email

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,45 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import Navigation from 'components/Navigation/Navigation';
-import AboutUs from 'components/tsx/AboutUs';
-import JoinUs from 'components/tsx/JoinUs';
-import TrainingAndMatches from 'components/tsx/TrainingAndMatches';
-import Footer from 'components/tsx/Footer';
-import {Element} from 'react-scroll';
-import teamphoto from '../assets/teamphoto.png';
-import FixturesAndTables from 'components/tsx/Fixtures/FixturesAndTables';
+import AboutUs from "components/tsx/AboutUs";
+import JoinUs from "components/tsx/JoinUs";
+import TrainingAndMatches from "components/tsx/TrainingAndMatches";
+import Footer from "components/tsx/Footer";
+import { Element } from "react-scroll";
+import teamphoto from "../assets/teamphoto.png";
+import FixturesAndTables from "components/tsx/Fixtures/FixturesAndTables";
 
-import './css/App.css';
+import "./css/App.css";
 
 class MainPage extends Component {
-
-
   render() {
     return (
       <div>
-          <Element className="element"name="aboutUs">
-            <AboutUs />
-          </Element>
-          <Element className="element">
-        <div className="teamPhoto">
-          <img src={teamphoto} alt="teamPhoto" className="img-fluid"/>
-        </div>
+        <Element className="element" name="aboutUs">
+          <AboutUs />
         </Element>
-          <Element className="element"name="training">
-            <TrainingAndMatches />
-          </Element>
+        <Element className="element">
+          <div className="teamPhoto">
+            <img src={teamphoto} alt="teamPhoto" className="img-fluid" />
+          </div>
+        </Element>
+        <Element className="element" name="training">
+          <TrainingAndMatches />
+        </Element>
 
-          <Element className="element"name="fixtures">
-            <FixturesAndTables />
-          </Element>
+        <Element className="element" name="fixtures">
+          <FixturesAndTables />
+        </Element>
 
-          <Element className="element"name="joinUs">
-            <JoinUs />
-          </Element>
+        <Element className="element" name="joinUs">
+          <JoinUs />
+        </Element>
         <Footer />
-        </div>
-
+      </div>
     );
   }
-
 }
 export default MainPage;
--- src/components/tsx/JoinUs.tsx
+++ src/components/tsx/JoinUs.tsx
@@ -1,90 +1,119 @@
-import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { Component, useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import "../css/JoinUs.css";
 import facebook from "../../assets/facebook.png";
 import instagram from "../../assets/instagram.png";
-class JoinUs extends Component {
-  handleSubmit(event) {
+const axios = require("axios").default;
+
+const blankForm = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  number: "",
+  message: "",
+};
+
+export const JoinUs = () => {
+  const [formData, setFormData] = useState(blankForm);
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(event);
-  }
+    axios
+      .post(
+        "https://wci6ukybl0.execute-api.eu-west-2.amazonaws.com/prod/contact-us",
+        {
+          ...formData,
+        }
+      )
+      .then((res) => {
+        alert("Form Submitted, we'll get back to you as soon as possible!");
+        console.log("Success");
+        console.log(res);
+        setFormData(blankForm);
+      })
+      .catch((err) => {
+        console.log("Error:");
+        console.log(err);
+        alert("Error: " + err);
 
-  render() {
-    return (
-      <Container fluid className="joinUs">
-        <Row className="h-100">
-          <Col md={1} className="left col-md-12 ">
-            <div>
-              <h2>Join Us</h2>
-              <p>
-                {" "}
-                At Camden we welcome everyone and pride ourselves on the good
-                vibes we bring, on and off the field. We would love to get in
-                touch so we can keep you updated and welcome you to the team!
-              </p>
-              <h2>Contact Us</h2>
-              <p className="bebas">Womens Team Co-Captains:</p>
-              <p>
-                Francesca Hutt
-                <br />
-                Annabelle Akintoye
-              </p>
-              <p className="bebas">Mens 1st Team Co-Captains:</p>
-              <p>
-                Austin Colgate
-                <br />
-                Travis Payne
-              </p>
-              <p className="bebas">Mens 2nd Team Co-Captains:</p>
-              <p>
-                Ross MacDonald
-                <br />
-                Alex Brodie
-              </p>
-              <p className="bebas"> Mens 3rd Team Co-Captains:</p>
-              <p>
-                Rob Hebden
-                <br />
-                Kane Roderique-Walker
-              </p>
-              <a href="https://www.facebook.com/CamdenLacrosse/">
-                <img alt="Facebook Page" src={facebook} />
-              </a>
-              <a href="https://www.instagram.com/camdencapybaras/">
-                <img alt="Instagram Page" src={instagram} />{" "}
-              </a>
-            </div>
-          </Col>
-          {/* <Col md={1} className="right col-md-6 d-flex align-items-center justify-content-center">
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridFirstName">
-                <Form.Control  placeholder="First Name" />
-              </Form.Group>
+        setFormData(blankForm);
+      });
+  };
 
-              <Form.Group as={Col} controlId="formGridLastName">
+  const onFormChange = (event) => {
+    setFormData({ ...formData, [event.target.id]: event.target.value });
+  };
 
-                <Form.Control placeholder="Surname" />
-              </Form.Group>
-            </Form.Row>
-
-            <Form.Group controlId="formGridEmail">
-              <Form.Control type="email" placeholder="Email Address"   />
+  return (
+    <Container fluid className="joinUs">
+      <Row className="h-100 justify-content-center text-a">
+        <div className="text">
+          <h2>Join Us</h2>
+          <p>
+            {" "}
+            At Camden we welcome everyone and pride ourselves on the good vibes
+            we bring, on and off the field. We would love to get in touch so we
+            can keep you updated and welcome you to the team!
+          </p>
+        </div>
+        <Form
+          onSubmit={(e) => handleSubmit(e)}
+          onChange={(e) => onFormChange(e)}
+        >
+          <Form.Row>
+            <Form.Group as={Col} controlId="firstName">
+              <Form.Control
+                value={formData.firstName}
+                placeholder="First Name"
+              />
             </Form.Group>
 
-            <Form.Group controlId="formGridNumber">
-              <Form.Control placeholder="Contact Number"  />
+            <Form.Group as={Col} controlId="lastName">
+              <Form.Control value={formData.lastName} placeholder="Surname" />
             </Form.Group>
-            <div className="d-flex justify-content-center">
+          </Form.Row>
+
+          <Form.Group controlId="email">
+            <Form.Control
+              value={formData.email}
+              type="email"
+              placeholder="Email Address"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="number">
+            <Form.Control
+              value={formData.number}
+              placeholder="Contact Number"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="message">
+            <Form.Control
+              value={formData.message}
+              as="textarea"
+              rows={5}
+              placeholder="Message"
+            />
+          </Form.Group>
+          <div className="d-flex justify-content-center">
             <Button variant="primary" type="submit">
               Submit
-          </Button>
+            </Button>
           </div>
-          </Form>
-        </Col>*/}
-        </Row>
-      </Container>
-    );
-  }
-}
+        </Form>
+      </Row>
+      <br />
+      <div className="text">
+        <a href="https://www.facebook.com/CamdenLacrosse/">
+          <img alt="Facebook Page" src={facebook} />
+        </a>
+        <a href="https://www.instagram.com/camdencapybaras/">
+          <img alt="Instagram Page" src={instagram} />{" "}
+        </a>
+      </div>
+    </Container>
+  );
+};
+
 export default JoinUs;
